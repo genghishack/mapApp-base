afaffc97d23a400cb4ab6b39d71f62db32238c15
Chris Wade <chrisw@brightervision.com>
2021-01-25 23:20:11 -0700

    Added basic form controls

--- src/components/EnterInfo/EnterInfo.tsx
+++ src/components/EnterInfo/EnterInfo.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './EnterInfo.scss';
+import { FormControl, InputLabel, Input, Button } from '@material-ui/core';
 
 const EnterInfo = () => {
     return (
         <div className="EnterInfo">
             <h1>Enter Info</h1>
+
+            <FormControl>
+                <InputLabel htmlFor="address">Address</InputLabel>
+                <Input id="address" aria-describedby="address input" />
+            </FormControl>
+
+            <Button variant="contained" color="primary">
+                Submit
+            </Button>
         </div>
     )
 }
