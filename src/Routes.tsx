4fe793e7808aefd6539de2d0b6906be38160f484
Chris Wade <chrisw@brightervision.com>
2021-02-02 22:15:10 -0700

    Added forgot password functionality

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
+import AuthenticatedRoute from "./components/Auth/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/Auth/UnauthenticatedRoute";
 import LoginView from "./components/views/LoginView";
 import SignupView from "./components/views/SignupView";
+import ResetPassword from "./components/Auth/ResetPassword";
 import NotFound from './components/views/NotFound';
 import ResourceView from "./components/views/ResourceView";
 import AboutView from "./components/views/AboutView";
 import EnterInfoView from "./components/views/EnterInfoView";
-import AuthenticatedRoute from "./components/Auth/AuthenticatedRoute";
-import UnauthenticatedRoute from "./components/Auth/UnauthenticatedRoute";
 
 const Routes = () => {
   return (
@@ -17,6 +18,9 @@ const Routes = () => {
       </Route>
       <UnauthenticatedRoute exact path="/login">
         <LoginView/>
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/login/reset">
+        <ResetPassword />
       </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/signup">
         <SignupView />
