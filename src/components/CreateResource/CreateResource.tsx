b6c9709afb47c5380a060dc58863627920bb4864
Chris Wade <genghishack@gmail.com>
2021-04-03 12:00:16 -0600

    Added business name, website, email, phone, fax fields

--- src/components/CreateResource/CreateResource.tsx
+++ src/components/CreateResource/CreateResource.tsx
@@ -21,6 +21,11 @@ const CreateResource = () => {
     country: 'US',
     postalCode: '',
     description: '',
+    website: '',
+    phone: '',
+    fax: '',
+    email: '',
+    business: '',
   });
 
   const validateForm = () => {
@@ -66,6 +71,51 @@ const CreateResource = () => {
             autoFocus
             type="text"
             value={fields.name}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        {/*@ts-ignore*/}
+        <Form.Group controlId="business">
+          <Form.Label>Business Name</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.business}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        {/*@ts-ignore*/}
+        <Form.Group controlId="website">
+          <Form.Label>Website</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.website}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        {/*@ts-ignore*/}
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        {/*@ts-ignore*/}
+        <Form.Group controlId="phone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.phone}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        {/*@ts-ignore*/}
+        <Form.Group controlId="fax">
+          <Form.Label>Fax</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.fax}
             onChange={handleFieldChange}
           />
         </Form.Group>
