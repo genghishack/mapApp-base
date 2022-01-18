e33d463ebc4273fde793e04c4fba8b87f774272a
Chris Wade <genghishack@gmail.com>
2022-01-17 19:32:52 -0700

    infoPanel displaying ok

--- src/components/InfoPanel/ResourceInfo.tsx
+++ src/components/InfoPanel/ResourceInfo.tsx
@@ -26,32 +26,44 @@ const ResourceInfo = () => {
     );
   }
 
+  const renderPhone = () => {
+    const {phone_json: phone} = resource;
+
+    return (
+      <>
+        {phone.main ? `${phone.main} (m)` : null}
+        {(phone.main && phone.cell) ? '/' : null}
+        {phone.cell ? `${phone.cell} (c)` : null}
+      </>
+    )
+  }
+
   return (
     <div className="ResourceInfo">
       {resource.id ? (
         <>
           <div className="name">
-            {resource.name}
+            {`${resource.name_json.first} ${resource.name_json.last}`}
           </div>
           <div className="infoSection business">
             <div className="label">Business</div>
-            {resource.business_name}
+            {resource.name_json.business}
           </div>
           <div className="infoSection website">
             <div className="label">Website</div>
-            {resource.website}
+            {resource.internet_json.web}
           </div>
           <div className="infoSection email">
             <div className="label">Email</div>
-            {resource.email}
+            {resource.internet_json.email}
           </div>
           <div className="infoSection phone">
             <div className="label">Phone</div>
-            {resource.phone}
+            {renderPhone()}
           </div>
           <div className="infoSection fax">
             <div className="label">Fax</div>
-            {resource.fax}
+            {resource.phone_json.fax}
           </div>
           <div className="infoSection address">
             <div className="label">Location</div>
